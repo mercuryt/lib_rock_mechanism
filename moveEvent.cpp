72cd819a70cb0a5fb12aa8c95bb23552c60e82f3
Mercury <alexcgilliland@gmail.com>
2023-04-07 11:20:22 -0700

    105 tests pass

--- moveEvent.cpp
+++ moveEvent.cpp
@@ -1,14 +1,23 @@
-MoveEvent::MoveEvent(uint32_t s, Actor* a) : ScheduledEvent(s), m_actor(a) { m_actor->m_taskEvent = this; }
+MoveEvent::MoveEvent(uint32_t s, Actor* a, u_int32_t dc) : ScheduledEvent(s), m_actor(a), m_delayCount(dc) { m_actor->m_taskEvent = this; }
 void MoveEvent::execute()
 {
 	Block* block = *(m_actor->m_routeIter);
 	if(block->anyoneCanEnterEver() && block->canEnterEver(m_actor))
 	{
-		//TODO: Request detour with canEnterCurrently pathing if waiting too long.
 		if(block->actorCanEnterCurrently(m_actor))
 		{
 			m_actor->m_routeIter++;
 			block->enter(m_actor);
+		}
+		else
+		{
+			m_delayCount++;
+			// Request detour with canEnterCurrently pathing if waiting too long.
+			if(m_delayCount == s_countOfDelaysBeforeDetour)
+			{
+				m_actor->m_location->m_area->registerRouteRequest(m_actor, true);
+				return;
+			}
 		}
 		if(block == m_actor->m_destination)
 		{
