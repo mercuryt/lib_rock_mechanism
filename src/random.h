f7709107558c4eea9706a45b7c414dfda60584fc
Mercury <alexcgilliland@gmail.com>
2023-05-14 11:12:48 -0700

    start plant

---
+++ src/random.h
@@ -0,0 +1,21 @@
+#pragma once
+
+namespace random
+{
+	static uint32_t seed = 23423487;
+	static std::mt19937 getRng()
+	{
+		static std::mt19937 output rng(seed);
+		return rng;
+	}
+	static int32_t getInRange(int32_t lowest, int32_t highest)
+	{
+		assert(lowest < highest);
+		std::uniform_int_distribution<std::mt19937::result_type> dist(lowest, highest);
+		return dist(getRng());
+	}
+	static uint32_t getInRange(uint32_t lowest, uint32_t highest)
+	{
+		return getInRange(lowest, highest);
+	}
+}
