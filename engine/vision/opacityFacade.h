20473a2f3ea5117f93363fa0c1dc1602937e2af3
Alex Gilliland <alexcgilliland@gmail.com>
2025-02-08 12:52:28 -0800

    chunk opacity data

--- engine/vision/opacityFacade.h
+++ engine/vision/opacityFacade.h
@@ -10,16 +10,16 @@ class Area;
 class OpacityFacade final
 {
 	Area& m_area;
-	StrongBitSet<BlockIndex> m_fullOpacity;
-	StrongBitSet<BlockIndex> m_floorOpacity;
+	StrongBitSet<BlockIndexChunked> m_fullOpacity;
+	StrongBitSet<BlockIndexChunked> m_floorOpacity;
 public:
 	OpacityFacade(Area& area);
 	void initalize();
 	void update(const BlockIndex& block);
 	void validate() const;
-	[[nodiscard]] bool isOpaque(const BlockIndex& index) const;
-	[[nodiscard]] bool floorIsOpaque(const BlockIndex& index) const;
+	[[nodiscard]] bool isOpaque(const BlockIndexChunked& index) const;
+	[[nodiscard]] bool floorIsOpaque(const BlockIndexChunked& index) const;
 	[[nodiscard]] bool hasLineOfSight(const BlockIndex& from, const BlockIndex& to) const;
-	[[nodiscard]] bool hasLineOfSight(const BlockIndex& fromIndex, Point3D fromCoords, Point3D toCoords) const;
-	[[nodiscard]] bool canSeeIntoFrom(const BlockIndex& previousIndex, const BlockIndex& currentIndex, const DistanceInBlocks& oldZ, const DistanceInBlocks& z) const;
+	[[nodiscard]] bool hasLineOfSight(const Point3D& fromCoords, const Point3D& toCoords) const;
+	[[nodiscard]] bool canSeeIntoFrom(const BlockIndexChunked& previousIndex, const BlockIndexChunked& currentIndex, const DistanceInBlocks& oldZ, const DistanceInBlocks& z) const;
 };
